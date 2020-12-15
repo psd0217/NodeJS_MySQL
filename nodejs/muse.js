dc8acddef69dae95f622736a4fc88f281fa225f3
박순영 <psd0217@naver.com>
2020-12-15 13:52:06 +0900

    setting

---
+++ nodejs/muse.js
@@ -0,0 +1,12 @@
+// var M = {
+//     v:'V',
+//     f:function(){
+//         console.log(this.v);
+//     }
+// }
+//M.f();
+
+const M = require('./mpart.js');
+var part = require('./mpart.js');
+console.log(part);
+part.f();
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports ={
+    id : 'soon',
+    password : '112233'
+}
