d5038908191645ed37c234316938cb23fe682662
박순영 <psd0217@naver.com>
2020-12-15 16:41:22 +0900

    코드 정리 topic2

--- main2.js
+++ main2.js
@@ -19,102 +19,16 @@ var app = http.createServer(function(request,response){
       }
       else
       {
-
-        //SELECT * FROM topic WHERE id = 3; 
-        db.query(`SELECT * FROM topic`, function(error,topics){
-          if(error)
-          {
-            throw error;
-          }
-          //`SELECT * FROM topic WHERE id = ${queryData.id}` 사용자의 공격이 가능한 코드임...
-          db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id = ?`,[queryData.id], function(error2,topic){
-            if(error2)
-            {
-              throw error2;
-            }
-            console.log(topic);
-            var title = topic[0].title;
-            var description = topic[0].description;
-            var list = template.list(topics);
-            var html = template.html(title, list,
-                `<h2>${title}</h2>
-                ${description}
-                <p>by ${topic[0].name}</p>`
-                ,
-                `<a href="/create">create</a>
-                 <a href="/update?id=${queryData.id}">update</a>
-                 <form action="delete_process" method="post">
-                   <input type="hidden" name="id" value="${queryData.id}">
-                   <input type="submit" value="delete">
-                 </form>`);
-            response.writeHead(200);
-            response.end(html);
-
-          });
-        });
-         
+        topic.page(request,response);
       }
-
     }
     else if(pathName === '/create')
     {
-      db.query(`SELECT * FROM topic`, function(error,topics){
-        if(error)
-        {
-          throw error;
-        }
-        db.query('SELECT * FROM author', function(error2,authors){
-          if(error2)
-          {
-            throw error2;
-          }
-          var title = 'Create';
-          var list = template.list(topics);
-          var html = template.html(title, list,
-              `
-              <form action="/create_process" method="post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-              `,
-              `<a href="/create">create</a>`);
-          response.writeHead(200);
-          response.end(html);
-        });
-        
-      });
+      topic.create(request,response);
     }
     else if(pathName === '/create_process')
     {
-      var body = '';
-      request.on('data',function(data){
-        body = body + data;
-      });
-      request.on('end',function(){
-        var post  = qs.parse(body);
-        // INSERT INTO topic (title, description, created, author_id)
-        // VALUES('Nodejs' ,'Nodejs is ...', NOW(), 1);
-          db.query(`
-          INSERT INTO topic (title, description, created, author_id) 
-          VALUES(?, ?, NOW(), ?)`,
-          [post.title, post.description, post.author],
-          function(error, result){
-            if(error)
-            {
-              throw error;
-            }
-            response.writeHead(302, {Location: `/?id=${result.insertId}`});
-            response.end();
-          });
-      });
+      topic.create_process(request,response);
     }
     else if(pathName === '/update')
     {
