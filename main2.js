f0fa7a57b5f72a1d72c8769f68198bbee4738199
박순영 <psd0217@naver.com>
2020-12-15 18:29:15 +0900

    저자 삭제, 삭제시 쓴 글도 같이 삭제

--- main2.js
+++ main2.js
@@ -60,6 +60,10 @@ var app = http.createServer(function(request,response){
     {
       author.update_process(request,response);
     }
+    else if(pathName === '/author/delete_process')
+    {
+      author.delete_process(request,response);
+    }
     else
     {
         response.writeHead(404);
