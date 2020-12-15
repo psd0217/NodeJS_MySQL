a4cdfd0b97df426edc205106fc8bdb2610982b98
박순영 <psd0217@naver.com>
2020-12-15 15:02:04 +0900

    페이지 DB적용

--- main2.js
+++ main2.js
@@ -5,7 +5,15 @@ var qs = require('querystring');
 var path = require('path');
 var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
 
+var db = mysql.createConnection({
+  host : 'localhost',
+  user : 'nodejs',
+  password : '111111',
+  database : 'opentutorials'
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -16,43 +24,72 @@ var app = http.createServer(function(request,response){
     {
       if(queryData.id === undefined)
       {
-        fs.readdir('./data', function(error, filelist)
-        {
+        db.query(`SELECT * FROM topic`, function(error,topics){
           var title = 'Welcome2';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.html(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`);
-            response.writeHead(200);
-            response.end(html);
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>`);
+          response.writeHead(200);
+          response.end(html);
         });
       }
       else
       {
-        fs.readdir('./data', function(error, filelist)
-        {
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err,description)
+        // fs.readdir('./data', function(error, filelist)
+        // {
+        //   var filteredId = path.parse(queryData.id).base;
+        //   fs.readFile(`data/${filteredId}`, 'utf8', function(err,description)
+        //   {
+        //     var title = queryData.id;
+        //     var sanitizedTitle = sanitizeHtml(title);
+        //     var sanitizedDescription = sanitizeHtml(description,{
+        //       allowedTags:['h1']
+        //     });
+        //     var list = template.list(filelist);
+        //     var html = template.html(sanitizedTitle, list,
+        //       `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        //       `<a href="/create">create</a>
+        //       <a href="/update?id=${sanitizedTitle}">update</a>
+        //       <form action="delete_process" method="post">
+        //         <input type="hidden" name="id" value="${sanitizedTitle}">
+        //         <input type="submit" value="delete">
+        //       </form>`);
+        //       response.writeHead(200);
+        //       response.end(html);
+        //   });
+        // });
+
+        //SELECT * FROM topic WHERE id = 3; 
+        db.query(`SELECT * FROM topic`, function(error,topics){
+          if(error)
           {
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description,{
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.html(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              `<a href="/create">create</a>
-              <a href="/update?id=${sanitizedTitle}">update</a>
-              <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${sanitizedTitle}">
-                <input type="submit" value="delete">
-              </form>`);
-              response.writeHead(200);
-              response.end(html);
+            throw error;
+          }
+          //`SELECT * FROM topic WHERE id = ${queryData.id}` 사용자의 공격이 가능한 코드임...
+          db.query(`SELECT * FROM topic WHERE id = ?`,[queryData.id], function(error2,topic){
+            if(error2)
+            {
+              throw error2;
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.html(title, list,
+                `<h2>${title}</h2>${description}`,
+                `<a href="/create">create</a>
+                 <a href="/update?id=${queryData.id}">update</a>
+                 <form action="delete_process" method="post">
+                   <input type="hidden" name="id" value="${queryData.id}">
+                   <input type="submit" value="delete">
+                 </form>`);
+            response.writeHead(200);
+            response.end(html);
+
           });
         });
+         
       }
 
     }
