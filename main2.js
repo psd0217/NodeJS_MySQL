7da97ffc66081fed16ad2359939f0654f3791f2a
박순영 <psd0217@naver.com>
2020-12-15 16:00:04 +0900

    저자를 적용해서 CREATE

--- main2.js
+++ main2.js
@@ -77,23 +77,37 @@ var app = http.createServer(function(request,response){
     else if(pathName === '/create')
     {
       db.query(`SELECT * FROM topic`, function(error,topics){
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.html(title, list,
-            `
-            <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-            `,
-            `<a href="/create">create</a>`);
-        response.writeHead(200);
-        response.end(html);
+        if(error)
+        {
+          throw error;
+        }
+        db.query('SELECT * FROM author', function(error2,authors){
+          if(error2)
+          {
+            throw error2;
+          }
+          var title = 'Create';
+          var list = template.list(topics);
+          var html = template.html(title, list,
+              `
+              <form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+              `,
+              `<a href="/create">create</a>`);
+          response.writeHead(200);
+          response.end(html);
+        });
+        
       });
     }
     else if(pathName === '/create_process')
@@ -109,7 +123,7 @@ var app = http.createServer(function(request,response){
           db.query(`
           INSERT INTO topic (title, description, created, author_id) 
           VALUES(?, ?, NOW(), ?)`,
-          [post.title, post.description, 1],
+          [post.title, post.description, post.author],
           function(error, result){
             if(error)
             {
