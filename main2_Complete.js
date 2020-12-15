5f902a6b496da103ebfd13e465ff42e52d8429ad
박순영 <psd0217@naver.com>
2020-12-15 16:29:07 +0900

    소스코드 정리 DB

--- main2_Complete.js
+++ main2_Complete.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 var url =  require('url');
 var qs = require('querystring');
+var path = require('path');
 var template = require('./lib/template.js');
-var db = require('./lib/db.js');
+var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
 
-
+var db = mysql.createConnection({
+  host : 'localhost',
+  user : 'nodejs',
+  password : '111111',
+  database : 'opentutorials'
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
